c0d022d28ea2badca12dad1cba02a19e95ed2112
Pat Thomas <pthomas@relaynetwork.com>
2015-11-05 09:29:14 -0500

    Initial commit.

---
+++ test/philly_show_checker/core_test.clj
@@ -0,0 +1,7 @@
+(ns philly-show-checker.core-test
+  (:require [clojure.test :refer :all]
+            [philly-show-checker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
