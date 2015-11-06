bb8e00f81cca6ba263246f4043b936d3281c74e9
Pat Thomas <pthomas@relaynetwork.com>
2015-11-05 19:56:08 -0500

    Dependency: quartzite.

--- src/philly_show_checker/core.clj
+++ src/philly_show_checker/core.clj
@@ -1,6 +1,2 @@
-(ns philly-show-checker.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns philly-show-checker.core
+  (:require [clojurewerkz.quartzite.scheduler :as qs]))
