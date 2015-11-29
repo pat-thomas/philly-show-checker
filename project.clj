d7ba53330e05b968a4af330031404b533d8d41fb
Pat Thomas <pthomas@relaynetwork.com>
2015-11-29 13:05:37 -0500

    Client-side dependencies.

--- project.clj
+++ project.clj
@@ -4,7 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main philly-show-checker.core
-  :dependencies [[org.clojure/clojure     "1.7.0"]
+  :dependencies [;; client-side
+                 [org.clojure/clojurescript "1.7.170"]
+                 [reagent "0.5.1"]
+
+                 ;; server-side
+                 [org.clojure/clojure     "1.7.0"]
                  [clojurewerkz/quartzite  "2.0.0"]
                  [com.draines/postal      "1.11.3"]
                  [org.clojure/tools.nrepl "0.2.12"]])
