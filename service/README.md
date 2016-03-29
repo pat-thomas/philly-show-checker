32a40d086cdd723d36e0dc9705c96b294a244c5c
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-03-29 11:03:51 -0400

    scalatra init build

---
+++ service/README.md
@@ -0,0 +1,12 @@
+# Philly Show Checker Service #
+
+## Build & Run ##
+
+```sh
+$ cd Philly_Show_Checker_Service
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ service/src/main/scala/com/ptsoftware/phillyshowchecker/service/PhillyShowCheckerServiceServlet.scala
@@ -0,0 +1,16 @@
+package com.ptsoftware.phillyshowchecker.service
+
+import org.scalatra._
+
+class PhillyShowCheckerServiceServlet extends PhillyShowCheckerServiceStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
