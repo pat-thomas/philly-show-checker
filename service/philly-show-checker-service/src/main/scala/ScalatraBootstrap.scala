45653b892a8d5140ff3dde79d41b5d1efd4c28a8
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-03-29 11:02:40 -0400

    clean stuff out

--- service/philly-show-checker-service/src/main/scala/ScalatraBootstrap.scala
+++
@@ -1,9 +0,0 @@
-import com.pt-software.philly-show-checker.service._
-import org.scalatra._
-import javax.servlet.ServletContext
-
-class ScalatraBootstrap extends LifeCycle {
-  override def init(context: ServletContext) {
-    context.mount(new PhillyShowCheckerServlet, "/*")
-  }
-}
